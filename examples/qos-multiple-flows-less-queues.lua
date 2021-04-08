88fa6470d918f946627513d754ad56a0347a48c9
Florian Wiedner <wiedner@net.in.tum.de>
2021-04-08 08:09:26 +0200

    Add random SrcPort

--- examples/qos-multiple-flows-less-queues.lua
+++ examples/qos-multiple-flows-less-queues.lua
@@ -94,7 +94,7 @@ local function fillUdpPacket(buf, len, port, dst_mac)
 		ethDst = dst_mac,
 		ip4Src = SRC_IP,
 		ip4Dst = DST_IP,
-		udpSrc = SRC_PORT,
+		udpSrc = math.random(0,65000),
 		udpDst = port,
 		pktLength = len
 	}
